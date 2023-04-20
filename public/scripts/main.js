cd3f1c9d9badbfc1c58033c90c3d7911b10c8206
rvdegroen <rvdegroen@gmail.com>
2023-04-20 08:19:55 +0200

    added camera

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,10 +16,39 @@ form.addEventListener("submit", (e) => {
 	}
 });
 
-// server
+// server chat
 socket.on("message", (msg) => {
 	const item = document.createElement("li");
 	item.textContent = msg;
 	messages.appendChild(item);
 	window.scrollTo(0, document.body.scrollHeight);
 });
+
+// video for face
+
+// source: https://usefulangle.com/post/352/javascript-capture-image-from-camera
+// WEB API's cannot be used in server.js
+const autodetectFoodButton = document.getElementById("autodetectFoodButton");
+const dishNameInput = document.getElementById("dishName");
+
+// you can draw on canvas
+const canvas = document.getElementById("canvas");
+const video = document.getElementById("video");
+
+// if mediaDevices is supported, remove hidden class
+if (navigator.mediaDevices) {
+	autodetectFoodButton.classList.remove("hidden");
+	autodetectFoodButton.addEventListener("click", async (e) => {
+		// e.preventdefault because any button in a form will automatically submit the form, but that's not what i want to do.
+		e.preventDefault();
+		canvas.classList.add("hidden");
+		video.classList.remove("hidden");
+		// permission to use media with only video | source: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: true,
+			audio: false,
+		});
+		// assign the camera to video | source
+		video.srcObject = stream;
+	});
+} // code will execute after 1000 milliseconds (1 second)
