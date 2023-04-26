a42e48d035b13c0cfd374b8a20aff0aa6e04f4dc
rvdegroen <rvdegroen@gmail.com>
2023-04-26 08:15:48 +0200

    added canvas to draw on

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,13 @@
+// VARIABLES
 // when it runs, it console.logs that the user connected from the server
 const socket = io();
-
-// VARIABLES
-const form = document.getElementById("form");
-const input = document.getElementById("input");
 const messages = document.getElementById("messages");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+// last known position for canvas
+const pos = { x: 0, y: 0 };
 
+// chat send button
 form.addEventListener("submit", (e) => {
 	// prevents the form from refreshing the page everytime you send a message
 	e.preventDefault();
@@ -24,28 +26,46 @@ socket.on("message", (msg) => {
 	window.scrollTo(0, document.body.scrollHeight);
 });
 
-// VIDEO
-// source: https://usefulangle.com/post/352/javascript-capture-image-from-camera
-// WEB API's cannot be used in server.js
-const $cameraButton = document.getElementById("camera");
-const $moodName = document.getElementById("mood__name");
-const $canvas = document.getElementById("canvas");
-const $video = document.getElementById("video");
+// var context = canvas.getContext("2d");
+// // pre-draw circle on canvas
+// context.beginPath();
+// context.arc(100, 100, 50, 0, 2 * Math.PI);
+// context.stroke();
 
-// if mediaDevices is supported, remove hidden class
-if (navigator.mediaDevices) {
-	$cameraButton.classList.remove("hidden");
-	$cameraButton.addEventListener("click", async (e) => {
-		// e.preventdefault because any button in a form will automatically submit the form, but that's not what i want to do.
-		e.preventDefault();
-		$canvas.classList.add("hidden");
-		$video.classList.remove("hidden");
-		// permission to use media with only video | source: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices
-		const stream = await navigator.mediaDevices.getUserMedia({
-			video: true,
-			audio: false,
-		});
-		// assign the camera to video | source
-		$video.srcObject = stream;
-	});
-} // code will execute after 1000 milliseconds (1 second)
+// context.fillStyle = "#FF0000";
+// context.fillRect(0, 0, 150, 75);
+
+// src: https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
+// new position from mouse event
+const setPosition = (e) => {
+	pos.x = e.clientX;
+	pos.y = e.clientY;
+};
+
+const draw = (e) => {
+	// mouse left button must be pressedm otherwise you draw without holding click
+	// 1 is the primary button (left mouse btn), 2 secondary, 3 primary + secondary
+	if (e.buttons !== 1) return;
+
+	ctx.beginPath(); // begin
+
+	ctx.lineWidth = 5;
+	ctx.lineCap = "round";
+	ctx.strokeStyle = "#c0392b";
+
+	ctx.moveTo(pos.x, pos.y); // from
+	setPosition(e);
+	ctx.lineTo(pos.x, pos.y); // to
+
+	ctx.stroke(); // draw it!
+};
+
+// pre-draw circle on canvas
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+// EVENT LISTENERS
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
